84cf22712f2fd5e767f1de700e631b651b56c91d
jigar-shethia <30865218+jigar-shethia@users.noreply.github.com>
2024-08-24 00:49:40 +0530

    First Commit

---
+++ DeCentralize/DeCentralizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeCentralizeApp.swift
+//  DeCentralize
+//
+//  Created by Jigar Shethia on 24/08/24.
+//
+
+import SwiftUI
+
+@main
+struct DeCentralizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
